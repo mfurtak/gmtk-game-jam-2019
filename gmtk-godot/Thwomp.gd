564646ec3f5cecc1fd5aae33909ae174652361c8
Michael Furtak <michael.furtak@gmail.com>
2019-08-02 19:03:38 -0400

    Adding raycasting to thwomp

--- gmtk-godot/Thwomp.gd
+++ gmtk-godot/Thwomp.gd
@@ -7,18 +7,21 @@ extends KinematicBody2D
 onready var player = get_node("/root/Main/Player")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if $LeftRayCast.is_colliding():
+		print("see you on the left!")
+	if $RightRayCast.is_colliding():
+		print("see you on the right!")
+
 
 
 func _physics_process(delta):
-	print(player.global_position)
-	print(get_position())
+	#print(player.global_position)
+	#print(get_position())
     # detect player overlap in x or y?
-	print("ASDF" + str(player))
+	#print("ASDF" + str(player))
 	#print("facebook.com" + str(player.translation))
 	pass
