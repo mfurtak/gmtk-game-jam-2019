3b38134fd40a1595d49aa19203e8ab73df15b108
Christopher Chestnut <cchestnut@villanova.edu>
2019-08-03 18:31:45 -0400

    game over transition to new game

--- gmtk-godot/GameOver.gd
+++ gmtk-godot/GameOver.gd
@@ -9,7 +9,12 @@ func _ready():
 	$Tween.interpolate_property($BlackMask, "color", Color(0, 0, 0, 1),
 	Color(0, 0, 0, 0),4,Tween.TRANS_LINEAR,Tween.EASE_IN)
 	$Tween.start()
+	$GameRestartTimer.start()
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_GameRestartTimer_timeout():	
+	get_tree().change_scene("res://menus/MainMenu.tscn")
