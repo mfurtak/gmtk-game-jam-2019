010a3d2f87c58d505b24e19fd4fbc653c7314ca4
Michael Furtak <michael.furtak@gmail.com>
2019-08-04 11:48:40 -0400

    Add gameover music and remove scream

--- gmtk-godot/GameOver.gd
+++ gmtk-godot/GameOver.gd
@@ -1,20 +1,10 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Tween.interpolate_property($BlackMask, "color", Color(0, 0, 0, 1),
 	Color(0, 0, 0, 0),4,Tween.TRANS_LINEAR,Tween.EASE_IN)
 	$Tween.start()
-	$GameRestartTimer.start()
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
-
-func _on_GameRestartTimer_timeout():	
+func _on_GameOverAudio_finished():
 	get_tree().change_scene("res://menus/MainMenu.tscn")
