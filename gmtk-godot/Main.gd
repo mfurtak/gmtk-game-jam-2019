051d20eb3816cb6cf46c5709c0789cc693a2e364
Michael Furtak <michael.furtak@gmail.com>
2019-08-04 13:00:45 -0400

    Add blob death sfx

--- gmtk-godot/Main.gd
+++ gmtk-godot/Main.gd
@@ -3,6 +3,15 @@ extends Node
 var current_level = "res://Level3.tscn"
 var level
 
+var blob_death_sfx = preload("res://audio/blob_death.wav")
+
+var sfx_library = {
+	"res://audio/blob_death.wav": blob_death_sfx
+}
+
+func get_sfx(res_path):
+	return sfx_library[res_path]
+
 func _ready():
 	# TODO(mfurtak) this is still needed?
 	for node in get_tree().get_nodes_in_group('ItemSwap'):
@@ -26,10 +35,18 @@ func initialize_level(level_file):
 	# Destroy all arrows that are stuck in places
 	for node in get_tree().get_nodes_in_group('projectile'):
 		node.queue_free()
+		
+	# Disconnect from anything that can request sfx
+	for node in get_tree().get_nodes_in_group('requests_sfx'):
+		node.disconnect("sfx_requested", self, "_on_sfx_requested")
 	
 	level = load(level_file).instance() 
 	level.set_name("CurrentLevel")
 	add_child(level)
+	
+		# Disconnect from anything that can request sfx
+	for node in get_tree().get_nodes_in_group('requests_sfx'):
+		node.connect("sfx_requested", self, "_on_sfx_requested")
 	
 	var exit = level.get_node("Exit")
 	if exit:
@@ -45,6 +62,13 @@ func initialize_level(level_file):
 func _on_exited(next_level):
 	print(next_level)
 	initialize_level(next_level)
+	
+func _on_sfx_requested(res_path, position):
+	if $SfxPlayer.playing:
+		$SfxPlayer.stop()
+	$SfxPlayer.stream = get_sfx(res_path)
+	$SfxPlayer.position = position
+	$SfxPlayer.play()
 
 func _on_shake_requested(duration = 0.2, frequency = 15, amplitude = 16):
 	$Camera2D/ScreenShake.start(duration, frequency, amplitude)
