e6dbf7ac4630872ab05dfb4282714c118ca5d159
Christopher Chestnut <cchestnut@villanova.edu>
2019-08-03 23:15:52 -0400

    making magic

--- gmtk-godot/LevelOmega.gd
+++ gmtk-godot/LevelOmega.gd
@@ -1,14 +1,17 @@
 extends Node2D
-var blob = load("res://Blob.tscn")
+var blob = "res://Thwomp.tscn"
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var newEnemy
 	for enemy in $TileMap/EnemySpawnPoints.get_children():
-		var newEnemy = blob.instance()
+		newEnemy = load(blob).instance()
+		add_child(newEnemy) 
 		newEnemy.set_position(enemy.position)
+		print(newEnemy)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
