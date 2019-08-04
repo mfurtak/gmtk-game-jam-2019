e46ea01588325fcd9af524b72d278b6c8fbdc7a3
Michael Furtak <michael.furtak@gmail.com>
2019-08-03 23:06:17 -0400

    Add a win screen and connect level 2 exit to it

--- gmtk-godot/Main.gd
+++ gmtk-godot/Main.gd
@@ -19,14 +19,26 @@ func _ready():
 
 func initialize_level(level_file):
 	if level != null:
-		level.get_node("Exit").disconnect("exited", self, "_on_exited")
+		var old_exit = level.get_node("Exit")
+		if old_exit:
+			old_exit.disconnect("exited", self, "_on_exited")
+			old_exit.disconnect("won", self, "_on_won")
 		level.queue_free()
+	
 	level = load(level_file).instance() 
 	level.set_name("CurrentLevel")
 	add_child(level)
-	level.get_node("Exit").connect("exited", self, "_on_exited")
-	$Player.set_position(level.get_node("PlayerSpawn").position)
-	$Player.connect("requests_shake", self, "_on_Player_requests_shake")
+	
+	var exit = level.get_node("Exit")
+	if exit:
+		exit.connect("exited", self, "_on_exited")
+		exit.connect("won", self, "_on_won")
+		
+	var spawn = level.get_node("PlayerSpawn")
+	if spawn:
+		$Player.set_position(spawn.position)
+	else:
+		$Player.set_position(Vector2(-64, -64))
 
 func _on_exited(next_level):
 	print(next_level)
@@ -34,6 +46,9 @@ func _on_exited(next_level):
 
 func _on_shake_requested(duration = 0.2, frequency = 15, amplitude = 16):
 	$Camera2D/ScreenShake.start(duration, frequency, amplitude)
+
+func _on_won():
+	get_tree().change_scene("res://YouWin.tscn")
 
 func _on_game_over():
 	get_tree().change_scene("res://GameOver.tscn")
