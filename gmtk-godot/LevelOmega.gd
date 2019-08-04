9d3291ef112c63d5c4a988d7b9eb060c25138fa5
Christopher Chestnut <cchestnut@villanova.edu>
2019-08-03 23:01:30 -0400

    pause

--- gmtk-godot/LevelOmega.gd
+++ gmtk-godot/LevelOmega.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+var blob = load("res://Blob.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -7,7 +7,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for enemy in $TileMap/EnemySpawnPoints.get_children():
-		
+		var newEnemy = blob.instance()
+		newEnemy.set_position(enemy.position)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
